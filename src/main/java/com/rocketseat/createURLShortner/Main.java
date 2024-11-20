770f8f54a1bb21a723dd1b64254bedb3c8118761
Luc4sf4 <superlucas2005@gmail.com>
2024-11-20 15:11:19 -0300

    Criando E configurando Projeto

---
+++ src/main/java/com/rocketseat/createURLShortner/Main.java
@@ -0,0 +1,15 @@
+package com.rocketseat.createURLShortner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
+        return Map.of();
+    }
+}
