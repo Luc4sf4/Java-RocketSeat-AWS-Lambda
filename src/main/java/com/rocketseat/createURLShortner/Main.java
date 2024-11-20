3f776e0a5f4dacbae64acea7cb0aa73d094ddc66
Luc4sf4 <superlucas2005@gmail.com>
2024-11-20 15:22:15 -0300

    Lógica das URLs

--- src/main/java/com/rocketseat/createURLShortner/Main.java
+++ src/main/java/com/rocketseat/createURLShortner/Main.java
@@ -2,14 +2,32 @@ package com.rocketseat.createURLShortner;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 import java.util.Map;
 import java.util.Objects;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return Map.of();
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = (String) input.get("body").toString();
+        
+
+        Map<String, String> bodyMap;
+        try {
+            bodyMap = objectMapper.readValue(body, Map.class);
+        } catch (JsonProcessingException exception) {
+            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
+        }
+
+        String originalUrl = bodyMap.get("originalURL");
+        String experationTime = bodyMap.get("expirationTime");
+
+        return null;
     }
 }
